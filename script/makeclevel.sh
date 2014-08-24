a9b3e1ee3c75b676cd9bef0179d1a5d3d7031a8d
OpenDcd <opendcd@users.noreply.github.com>
2014-08-24 18:50:30 +0100

    Added Makefile for Kaldi converters

--- script/makeclevel.sh
+++ script/makeclevel.sh
@@ -24,24 +24,32 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/fst/
 
 mkdir -p ${GRAPH}
 
-if [ 1 == 1 ]; then
-  fstdeterminize ${LANG}/L_disambig.fst  ${GRAPH}/det.L.fst
+#
+fstdeterminize ${LANG}/L_disambig.fst  ${GRAPH}/det.L.fst
 
+#
 ${KALDI_ROOT}/src/fstbin/fstcomposecontext \
   --context-size=3 --central-position=1 --binary=false \
   --read-disambig-syms=${LANG}/phones/disambig.int \
   --write-disambig-syms=${GRAPH}/disambig_ilabels_3_1.int \
   ${GRAPH}/ilabels_3_1 ${GRAPH}/det.L.fst | fstarcsort - ${GRAPH}/C.det.L.fst
 
-${KALDI_ROOT}/src/fstbin/fstrmsymbols ${GRAPH}/disambig_ilabels_3_1.int \
-  ${GRAPH}/C.det.L.fst - | fstconvert --fst_type=olabel_lookahead \
+#Generate the arcs files and logical to physical mapping
+../src/kaldibin/make-arc-types ${GRAPH}/ilabels_3_1 \
+  ${EXP}/tree ${EXP}/final.mdl ${GRAPH}/arcs.far ${GRAPH}/log2phys
+
+#Relabel the input
+fstrelabel --relabel_ipairs=${GRAPH}/log2phys ${GRAPH}/C.det.L.fst - \
+  ${KALDI_ROOT}/src/fstbin/fstrmsymbols ${GRAPH}/disambig_ilabels_3_1.int \
+  - - | fstconvert --fst_type=olabel_lookahead \
   --save_relabel_opairs=${GRAPH}/g.irelabel ${GRAPH}/C.det.L.fst \
   ${GRAPH}/la.C.det.L.fst
 
-fi
-
+#
 fstrelabel --relabel_opairs=${GRAPH}/g.irelabel ${LANG}/G.fst \
   | fstarcsort | fstconvert --fst_type=const - ${GRAPH}/G.fst
 
+#
 fstcompose ${GRAPH}/la.C.det.L.fst ${GRAPH}/G.fst | \
   fstconvert --fst_type=const - ${GRAPH}/C.det.L.G.fst 
+
