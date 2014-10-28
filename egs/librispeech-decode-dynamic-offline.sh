0039b4becf68d66548f547bc63fbf995c43b5062
Paul Dixon <paul.r.dixon@gmail.com>
2014-10-28 23:06:31 +0100

    Rename dcd.mk to darwin.mk

--- egs/librispeech-decode-dynamic-offline.sh
+++ egs/librispeech-decode-dynamic-offline.sh
@@ -7,7 +7,7 @@
   --decoder_type=hmm_lattice \
   --beam=15 \
   --acoustic_scale=0.1 \
-  --fst_reset_period=1 \
+  --fst_reset_period=50 \
   graph_test_tgsmall/arcs.far \
   graph_test_tgsmall/la.C.det.L.fst,graph_test_tgsmall/G.fst \
   ark:test-clean.ark  recog.far
