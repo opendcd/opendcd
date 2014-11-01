9912ae63910ad50c7ff161f16e4fb584eab03415
edobashira <dixonp@dixonp-MS-7522.(none)>
2014-11-01 23:52:00 +0100

    Get dev reference files

--- egs/librispeech.sh
+++ egs/librispeech.sh
@@ -60,6 +60,8 @@ if [ ! -f dev-clean.tar.gz ]; then
   wget http://www.openslr.org/resources/12/dev-clean.tar.gz
   tar -zxf dev-clean.tar.gz
   curl http://www.kaldi-asr.org/downloads/build/6/trunk/egs/librispeech/s5/data/dev_clean/archive.tar.gz  -o dev_clean_lists.tar.gz
+  curl http://www.kaldi-asr.org/downloads/build/6/trunk/egs/librispeech/s5/exp/nnet2_online/nnet_a_online/decode_dev_clean_tgsmall/scoring/test_filt.txt \
+    -o ./librispeech/dev_clean/dev_clean/test_filt.txt
 fi
 
 #Build the medium sized graph
