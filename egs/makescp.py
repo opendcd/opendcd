47bc0d1a41d70148abcc3c817f1c7d7cc1077f67
edobashira <dixonp@dixonp-MS-7522.(none)>
2014-11-01 23:43:24 +0100

    Build into 3rdparty/local and librispeech dev-clean tests

--- egs/makescp.py
+++ egs/makescp.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 list_of_files = {}
-for (dirpath, dirnames, filenames) in os.walk("LibriSpeech/test-clean/1089/134686/"):
+for (dirpath, dirnames, filenames) in os.walk(sys.argv[1]):
   for filename in filenames:
     if filename[-5:] == '.flac': 
       print "%s  flac -c -d -s %s |"% (filename[:-5], os.sep.join([dirpath, filename]))
