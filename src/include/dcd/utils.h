5c14d29be9478dea75c7ff4ad64382d22b43b409
Paul Dixon <paul.r.dixon@gmail.com>
2014-11-17 18:44:37 +0100

    Small changes

--- src/include/dcd/utils.h
+++ src/include/dcd/utils.h
@@ -12,6 +12,7 @@
 // limitations under the License.
 //
 // Copyright 2013-2014 Yandex LLC
+// Copyright 2014 Paul R. Dixon
 // Author : Paul R. Dixon (info@opendcd.org)
 // \file
 // Various helper function and ultities
