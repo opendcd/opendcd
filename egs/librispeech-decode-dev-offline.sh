8912918443e0fe04577c226cc523cc20c964f2ed
Paul Dixon <paul.r.dixon@gmail.com>
2014-11-30 21:53:56 -0800

    Merge master

--- egs/librispeech-decode-dev-offline.sh
+++ egs/librispeech-decode-dev-offline.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
-#Script to deccode the segments and score the outputs
+# Copyright 2014 Paul R. Dixon (info@opendcd.org)
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+# WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+# MERCHANTABLITY OR NON-INFRINGEMENT.
+# See the Apache 2 License for the specific language governing permissions and
+# limitations under the License.
+
+# script to decode the pre-computed nnets segments and
+# score the outputs, useful for tuning and benchmarking
+# the system
+
+
 b=15
 for f in  aa ab ac ad ;
-do 
+do
  ../src/bin/dcd-recog \
   --word_symbols_table=words.txt \
   --decoder_type=hmm_lattice \
